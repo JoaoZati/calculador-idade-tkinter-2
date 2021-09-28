82e39d1970220839257173bd1a37a48f9d2fabd0
JoaoZati <joaozati@gmail.com>
2021-09-28 09:03:15 -0300

    Commit 1: - Reformatado organização dos frames

--- calculadora-idade.py
+++ calculadora-idade.py
@@ -12,12 +12,22 @@ class CalculadoraIdade:
         self.window.title('Calculadora Idade')
         self.window.geometry('310x410')
 
-        self.frame_cima = tk.Frame(self.window, width=310, height=140, pady=0, padx=0,
-                                   relief=tk.FLAT, bg=COR1)
-        self.frame_cima.grid(row=0, column=0)
-        self.frame_baixo = tk.Frame(self.window, width=310, height=300, pady=0, padx=0,
-                                    relief=tk.FLAT, bg=COR2)
-        self.frame_baixo.grid(row=1, column=0)
+        self.frame_cima = self.frame_cima()
+        self.frame_baixo = self.frame_baixo()
+
+    def frame_cima(self):
+        frame_cima = tk.Frame(self.window, width=310, height=140, pady=0, padx=0,
+                              relief=tk.FLAT, bg=COR1)
+        frame_cima.grid(row=0, column=0)
+
+        return frame_cima
+
+    def frame_baixo(self):
+        frame_baixo = tk.Frame(self.window, width=310, height=300, pady=0, padx=0,
+                               relief=tk.FLAT, bg=COR2)
+        frame_baixo.grid(row=1, column=0)
+
+        return frame_baixo
 
     def run(self):
         self.window.mainloop()
